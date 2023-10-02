4438855e5f689d0c60b37e4862eb89aa8f4068ca
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-10-01 21:46:20 -0700

    added test source set and deps

--- pendant-starlark/build.gradle.kts
+++ pendant-starlark/build.gradle.kts
@@ -45,10 +45,11 @@ kotlin {
 
 
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
+        val jvmMain by getting
+        val jvmTest by getting {
             dependencies {
-                implementation(kotlin("test"))
+                implementation(libs.kotest.junit5)
+                implementation(libs.kotest.assertions)
             }
         }
     }
