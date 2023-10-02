48c3a2820c51934aef34deb1e821600cd71bc027
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-10-01 19:26:43 -0700

    configure version catalog, gradle modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,23 @@
 pluginManagement {
     repositories {
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
 rootProject.name = "pendant"
+include(":pendant-starlark")
