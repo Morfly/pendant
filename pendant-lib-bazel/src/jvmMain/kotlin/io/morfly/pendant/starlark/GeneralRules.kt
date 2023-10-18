92e9ce2b793a5771677006c2d0b3f10bebd0b500
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-10-18 15:21:58 -0700

    updated LICENSE

--- pendant-lib-bazel/src/jvmMain/kotlin/io/morfly/pendant/starlark/GeneralRules.kt
+++ pendant-lib-bazel/src/jvmMain/kotlin/io/morfly/pendant/starlark/GeneralRules.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Pavlo Stavytskyi
+ * Copyright 2023 Pavlo Stavytskyi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
