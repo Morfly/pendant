dd7b405edff9d3385d4f5b225af35e19adcc033f
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-10-18 15:44:37 -0700

    improved empty tuple

--- pendant-starlark/src/jvmMain/kotlin/io/morfly/pendant/starlark/lang/type/Tuple.kt
+++ pendant-starlark/src/jvmMain/kotlin/io/morfly/pendant/starlark/lang/type/Tuple.kt
@@ -44,12 +44,7 @@ fun List<Any?>.toTuple(): Tuple = TupleImpl(this)
 @JvmInline
 internal value class TupleImpl(override val items: List<Any?>) : Tuple
 
-internal object EmptyTuple : Tuple {
-    override val items: List<Any?> = emptyList()
-}
-
 internal fun tupleOf(vararg items: Any?): Tuple =
     TupleImpl(items.toList())
 
-internal fun emptyTuple(): Tuple =
-    EmptyTuple
+internal fun emptyTuple(): Tuple = TupleImpl(emptyList())
