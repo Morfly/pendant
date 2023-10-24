c832265e6fc3f7d742d8317fec7086cc6af8a9b5
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-10-24 15:29:56 -0700

    add dagger rules

---
+++ pendant-library-bazel/src/jvmMain/kotlin/io/morfly/pendant/starlark/DaggerRules.kt
@@ -0,0 +1,27 @@
+package io.morfly.pendant.starlark
+
+import io.morfly.pendant.starlark.lang.Argument
+import io.morfly.pendant.starlark.lang.FunctionKind
+import io.morfly.pendant.starlark.lang.FunctionScope
+import io.morfly.pendant.starlark.lang.LibraryFunction
+
+@LibraryFunction(
+    name = "dagger_rules",
+    scope = [FunctionScope.Build],
+    kind = FunctionKind.Statement
+)
+private interface DaggerRules
+
+@LibraryFunction(
+    name = "dagger_android_rules",
+    scope = [FunctionScope.Build],
+    kind = FunctionKind.Statement
+)
+private interface DaggerAndroidRules
+
+@LibraryFunction(
+    name = "hilt_android_rules",
+    scope = [FunctionScope.Build],
+    kind = FunctionKind.Statement
+)
+private interface HiltAndroidRules
